716434fb8639af5f270d04fef3f6476960517966
fbosio <fedebosio93@gmail.com>
2019-12-14 16:12:51 -0300

    Bouncing ball test made (tutorial)

--- main.lua
+++ main.lua
@@ -5,8 +5,27 @@ require("timeline")
 require("place")
 
 function love.load()
-  world = love.physics.newWorld(0, 980.665)
-  body = love.physics.newBody(world, 400, 0, "dynamic")
+  meterUnitInPx = 250
+  love.physics.setMeter(meterUnitInPx)
+  world = love.physics.newWorld(0, 9.80665*meterUnitInPx)
+
+  local width, height, _ = love.window.getMode()
+  physicalObjects = {
+    ground = {
+      -- Bodies are "static" by default
+      body = love.physics.newBody(world, width/2, height-120),
+      shape = love.physics.newRectangleShape(width, 100),
+    },
+    ball = {
+      body = love.physics.newBody(world, width/2, 0, "dynamic"),
+      shape = love.physics.newCircleShape(20),
+    }
+  }
+  physicalObjects.ground.fixture = love.physics.newFixture(
+    physicalObjects.ground.body, physicalObjects.ground.shape)
+  physicalObjects.ball.fixture = love.physics.newFixture(
+    physicalObjects.ball.body, physicalObjects.ball.shape)
+  physicalObjects.ball.fixture:setRestitution(0.7)
 
   player1 = Player:new(400, 300)
   player2 = Player:new(100, 300)
@@ -25,18 +44,26 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.setColor(1, 1, 1)
   player1:draw()
   player2:draw()
 
   -- Physics test
-  love.graphics.circle("fill", body:getX(), body:getY(), 2)
+  love.graphics.setColor(0, 0.5, 0)
+  love.graphics.polygon("fill", physicalObjects.ground.body:getWorldPoints(
+    physicalObjects.ground.shape:getPoints()))
+  love.graphics.setColor(1, 1, 0)
+  love.graphics.circle("fill", physicalObjects.ball.body:getX(),
+    physicalObjects.ball.body:getY(), physicalObjects.ball.shape:getRadius())
 
   -- Text placement example
   local x = 200
   local anchors = {"north", "center", "south west", "east"}
   for j, anchor in ipairs(anchors) do
 	  local y = (190*j-200+10*#anchors) / (#anchors-1)
+    love.graphics.setColor(1, 1, 1)
 	  place.textByAnchor("Testing " .. anchor .. " anchor", x, y, anchor)
+    love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", x, y, 2);
   end
 end
