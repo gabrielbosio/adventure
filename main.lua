8d5e9cc274e2f64772f277c5f53ef617b290a905
gabrielbosio <gabrielbosio95@gmail.com>
2020-01-09 18:17:00 -0300

    Remove background cutscene and text

--- main.lua
+++ main.lua
@@ -1,19 +1,8 @@
-require("animation")
-require("animationplayer")
 require("collision")
 require("levels")
-require("player")
-require("timeline")
 require("place")
 
 function love.load()
-  player1 = Player:new(400, 300)
-  player2 = Player:new(100, 300)
-  timeline = Timeline:new()
-  timeline:addKeyFrame(2, function () player1:walk(5, true) end)
-  timeline:addKeyFrame(3, function () player2:walk(8, true) end)
-  timeline:addKeyFrame(4, function () player1:walk(3) end)
-
   -- Physics test
   xSpeed = 500
   jumpImpulseSpeed = 1400
@@ -42,9 +31,6 @@ end
 
 function love.update(dt)
   local playerBox = components.collisionBoxes.megasapi
-  player1:update(dt)
-  player2:update(dt)
-  timeline:update(dt)
 
   -- X Movement Input (far simpler than the callback approach)
   if love.keyboard.isDown("a") and not love.keyboard.isDown("d") then
@@ -61,19 +47,6 @@ end
 function love.draw()
   local playerBox = components.collisionBoxes.megasapi
   love.graphics.setColor(1, 1, 1)
-  player1:draw()
-  player2:draw()
-
-  -- Text placement example
-  local x = 200
-  local anchors = {"north", "center", "south west", "east"}
-  for j, anchor in ipairs(anchors) do
-	  local y = (190*j-200+10*#anchors) / (#anchors-1)
-    love.graphics.setColor(1, 1, 1)
-	  place.textByAnchor("Testing " .. anchor .. " anchor", x, y, anchor)
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", x, y, 2);
-  end
 
   -- Physics test
   levels.drawTerrainOutline()
