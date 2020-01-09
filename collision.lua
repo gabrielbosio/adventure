e96fc2e5b75a7645406f7a36776d3f94b8832de2
gabrielbosio <gabrielbosio95@gmail.com>
2020-01-09 18:09:17 -0300

    Extract collision and movement logic into terrain collision system

---
+++ collision.lua
@@ -0,0 +1,192 @@
+module("collision", package.seeall)
+
+
+function terrainCollision(components, terrain, dt)
+  local gravity = 5000
+  
+  for entity, solidComponent in pairs(components.solids) do
+    local collisionBox = components.collisionBoxes[entity]
+    collisionBox.vy = collisionBox.vy + gravity*dt
+
+    for i in pairs(terrain.boundaries) do
+      local boundaries = terrain.boundaries[i]
+      local x1 = math.min(boundaries[1], boundaries[3])
+      local y1 = math.min(boundaries[2], boundaries[4])
+      local x2 = math.max(boundaries[1], boundaries[3])
+      local y2 = math.max(boundaries[2], boundaries[4])
+
+      -- Bottom
+      if collisionBox.x + collisionBox.width/2 > x1 and collisionBox.x - collisionBox.width/2 < x2
+          and collisionBox.y + collisionBox.vy*dt > y1
+          and collisionBox.y - collisionBox.height/2 < y2 then
+        collisionBox.vy = 0
+        collisionBox.y = y1
+      end
+
+      -- Top
+      if collisionBox.x + collisionBox.width/2 > x1 and collisionBox.x - collisionBox.width/2 < x2
+          and collisionBox.y - collisionBox.height + collisionBox.vy*dt < y2
+          and collisionBox.y > y1 then
+        collisionBox.vy = 0
+        collisionBox.y = y2 + collisionBox.height
+      end
+
+      -- Left
+      if (collisionBox.x - collisionBox.width/2 + collisionBox.vx*dt < x2
+          and collisionBox.x + collisionBox.width/2 > x1
+          and collisionBox.y - collisionBox.height < y2 and collisionBox.y > y1 
+          and not collisionBox.onSlope) then
+          --[[or (collisionBox.onSlope and collisionBox.x + collisionBox.vx*dt < x2
+          and collisionBox.x > x1
+          and collisionBox.y - collisionBox.height < y2 and collisionBox.y > y1) then]]
+        collisionBox.vx = 0
+        collisionBox.x = x2 + collisionBox.width/2
+      end
+
+      -- Right
+      if collisionBox.x + collisionBox.width/2 + collisionBox.vx*dt > x1
+          and collisionBox.x - collisionBox.width/2 < x2
+          and collisionBox.y - collisionBox.height < y2 and collisionBox.y > y1 
+          and not collisionBox.onSlope then
+        collisionBox.vx = 0
+        collisionBox.x = x1 - collisionBox.width/2
+      end
+    end
+
+    -- Slopes
+    collisionBox.onSlope = false
+
+    for i in pairs(terrain.slopes) do
+      local x1, y1, x2, y2 = unpack(terrain.slopes[i])
+      local yTop, yBottom = math.min(y1, y2), math.max(y1, y2)
+
+      -- Right angle side (acts like a wall)
+      if collisionBox.y > yTop and collisionBox.y - collisionBox.height < yBottom then
+        if x1 > x2 and collisionBox.x + collisionBox.width/2 <= x2
+            and collisionBox.x + collisionBox.width/2 + collisionBox.vx*dt > x2 then
+          collisionBox.vx = 0
+          collisionBox.x = x2 - collisionBox.width/2
+        elseif x1 < x2 and collisionBox.x - collisionBox.width/2 >= x2
+            and collisionBox.x - collisionBox.width/2 + collisionBox.vx*dt < x2 then
+          collisionBox.vx = 0
+          collisionBox.x = x2 + collisionBox.width/2
+        end
+      end
+
+      -- Sharp corner (acts like a wall)
+      if x1 > x2 and collisionBox.x + collisionBox.width/2 > x1
+          and collisionBox.x - collisionBox.width/2 + collisionBox.vx*dt < x1 then
+        if (y1 > y2 and collisionBox.y > y1
+            and collisionBox.y - collisionBox.height < y1) or (y1 < y2
+            and collisionBox.y > y1 and collisionBox.y - collisionBox.height < y1) then
+          collisionBox.vx = 0
+          collisionBox.x = x1 + collisionBox.width/2
+        end
+      elseif x1 < x2 and collisionBox.x - collisionBox.width/2 < x1
+          and collisionBox.x + collisionBox.width/2 + collisionBox.vx*dt > x1 then
+        if (y1 > y2 and collisionBox.y > y1
+            and collisionBox.y - collisionBox.height < y1) or (y1 < y2
+            and collisionBox.y > y1 and collisionBox.y - collisionBox.height < y1) then
+          collisionBox.vx = 0
+          collisionBox.x = x1 - collisionBox.width/2
+        end
+      end
+
+      -- Top and bottom
+      if collisionBox.x + collisionBox.width/2 > math.min(x1, x2)
+          and collisionBox.x - collisionBox.width/2 < math.max(x1, x2) then
+        local m = (y2-y1) / (x2-x1)
+
+        if y1 < y2 then
+          -- Ceiling (leaning)
+          if collisionBox.y - collisionBox.height >= y1
+              and collisionBox.y - collisionBox.height <= y2 then
+            slopeX = x1 + (collisionBox.y-collisionBox.height-y1)/m
+
+            if x1 > x2 and collisionBox.x - collisionBox.width/2 >= slopeX
+                and collisionBox.x - collisionBox.width/2 + collisionBox.vx*dt < slopeX
+                then
+              collisionBox.vx = 0
+              collisionBox.x = slopeX + collisionBox.width/2
+            elseif x1 < x2 and collisionBox.x + collisionBox.width/2 <= slopeX
+                and collisionBox.x + collisionBox.width/2 + collisionBox.vx*dt > slopeX
+                then
+              collisionBox.vx = 0
+              collisionBox.x = slopeX - collisionBox.width/2
+            end
+          end
+
+          local slopeY = y1 + m*(collisionBox.x-x1)
+
+          if collisionBox.y - collisionBox.height >= slopeY
+              and collisionBox.y - collisionBox.height + collisionBox.vy*dt < slopeY
+              then
+            collisionBox.vy = 0
+            collisionBox.y = slopeY + collisionBox.height
+          end
+
+          -- Floor (flat)
+          if collisionBox.y + collisionBox.vy*dt > y1 and collisionBox.y <= y1 then
+            collisionBox.vy = 0
+            collisionBox.y = y1
+          end
+
+        else  -- (y1 >= y2)
+
+          -- Ceiling (flat) 
+          if collisionBox.vy < 0 and collisionBox.y - collisionBox.height >= y1
+              and collisionBox.y - collisionBox.height + collisionBox.vy*dt < y1 then
+            collisionBox.vy = 0
+            collisionBox.y = y1 + collisionBox.height
+          end
+
+          -- Floor (leaning)
+          local slopeY = y1 + m*(collisionBox.x-x1)
+          if collisionBox.y <= math.max(y2, slopeY)
+              and collisionBox.y + collisionBox.vy*dt > math.max(y2, slopeY) then
+            collisionBox.vy = 0
+
+            -- Prevent floating character at slope corner
+            if (collisionBox.x-x1)*(collisionBox.x-x2) < 0 then
+              collisionBox.y = slopeY
+            else
+              collisionBox.y = y2
+            end
+          end
+
+          if collisionBox.y <= y1 and collisionBox.y > math.max(y2, slopeY)
+              and ((x1 > x2 and collisionBox.x - collisionBox.width/2 <= x1
+                    and collisionBox.x + collisionBox.width/2 >= x2)
+                    or (x1 < x2 and collisionBox.x + collisionBox.width/2 >= x1
+                    and collisionBox.x - collisionBox.width/2 <= x2)) then
+            collisionBox.vy = 0
+            collisionBox.y = slopeY
+            collisionBox.onSlope = true
+          end
+
+
+        end -- if y1 < y2
+      end -- if collisionBox.x + collisionBox.width/2 > math.min(x1, x2)
+
+    end  -- for i
+
+    -- Clouds
+    for i in pairs(terrain.clouds) do
+      local clouds = terrain.clouds[i]
+      local x1 = math.min(clouds[1], clouds[3])
+      local y1 = clouds[2]
+      local x2 = math.max(clouds[1], clouds[3])
+
+      if collisionBox.x + collisionBox.width/2 > x1 and collisionBox.x - collisionBox.width/2 < x2
+          and collisionBox.y + collisionBox.vy*dt > y1 and collisionBox.y <= y1
+          and collisionBox.vy > 0 then
+        collisionBox.vy = 0
+        collisionBox.y = y1
+      end
+    end
+
+    local winWidth, winHeight = love.window.getMode()
+    collisionBox.x = (collisionBox.x+collisionBox.vx*dt)
+    collisionBox.y = (collisionBox.y+collisionBox.vy*dt)
+  end
+end
--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require("animation")
 require("animationplayer")
+require("collision")
 require("player")
 require("timeline")
 require("place")
@@ -14,12 +15,17 @@ function love.load()
   timeline:addKeyFrame(4, function () player1:walk(3) end)
 
   -- Physics test
-  gravity = 5000
   xSpeed = 500
   jumpImpulseSpeed = 1400
 
-  playerBox = {x = 46, y = 142, vx = 0, vy = 0, width = 50, height = 100}
-
+  components = {
+    collisionBoxes = {
+      megasapi = {x = 46, y = 142, vx = 0, vy = 0, width = 50, height = 100, onSlope = false}
+    },
+    solids = {
+      megasapi = {}
+    }
+  }
 
   cloudHeightDisplay = 10 
 end
@@ -27,6 +33,7 @@ end
 -- Input test - callbacks seem good for prevent bunny hops
 -- not good for horizontal movement though: one need keyboard.isDown anyway
 function love.keypressed(key, isrepeat)
+  local playerBox = components.collisionBoxes.megasapi
   -- Y Movement Input
   if key == "w" and playerBox.vy == 0 then
     playerBox.vy = -jumpImpulseSpeed
@@ -34,6 +41,7 @@ function love.keypressed(key, isrepeat)
 end
 
 function love.update(dt)
+  local playerBox = components.collisionBoxes.megasapi
   player1:update(dt)
   player2:update(dt)
   timeline:update(dt)
@@ -47,192 +55,11 @@ function love.update(dt)
     playerBox.vx = 0
   end
 
-  -- Physics test
-  playerBox.vy = playerBox.vy + gravity*dt
-
-  for i in pairs(map.terrain.boundaries) do
-    local boundaries = map.terrain.boundaries[i]
-    local x1 = math.min(boundaries[1], boundaries[3])
-    local y1 = math.min(boundaries[2], boundaries[4])
-    local x2 = math.max(boundaries[1], boundaries[3])
-    local y2 = math.max(boundaries[2], boundaries[4])
-
-    -- Bottom
-    if playerBox.x + playerBox.width/2 > x1 and playerBox.x - playerBox.width/2 < x2
-        and playerBox.y + playerBox.vy*dt > y1
-        and playerBox.y - playerBox.height/2 < y2 then
-      playerBox.vy = 0
-      playerBox.y = y1
-    end
-
-    -- Top
-    if playerBox.x + playerBox.width/2 > x1 and playerBox.x - playerBox.width/2 < x2
-        and playerBox.y - playerBox.height + playerBox.vy*dt < y2
-        and playerBox.y > y1 then
-      playerBox.vy = 0
-      playerBox.y = y2 + playerBox.height
-    end
-
-    -- Left
-    if (playerBox.x - playerBox.width/2 + playerBox.vx*dt < x2
-        and playerBox.x + playerBox.width/2 > x1
-        and playerBox.y - playerBox.height < y2 and playerBox.y > y1 
-        and not guyOnSlope) then
-        --[[or (guyOnSlope and playerBox.x + playerBox.vx*dt < x2
-        and playerBox.x > x1
-        and playerBox.y - playerBox.height < y2 and playerBox.y > y1) then]]
-      playerBox.vx = 0
-      playerBox.x = x2 + playerBox.width/2
-    end
-
-    -- Right
-    if playerBox.x + playerBox.width/2 + playerBox.vx*dt > x1
-        and playerBox.x - playerBox.width/2 < x2
-        and playerBox.y - playerBox.height < y2 and playerBox.y > y1 
-        and not guyOnSlope then
-      playerBox.vx = 0
-      playerBox.x = x1 - playerBox.width/2
-    end
-  end
-
-  -- Slopes
-  guyOnSlope = false
-
-  for i in pairs(map.terrain.slopes) do
-    local x1, y1, x2, y2 = unpack(map.terrain.slopes[i])
-    local yTop, yBottom = math.min(y1, y2), math.max(y1, y2)
-
-    -- Right angle side (acts like a wall)
-    if playerBox.y > yTop and playerBox.y - playerBox.height < yBottom then
-      if x1 > x2 and playerBox.x + playerBox.width/2 <= x2
-          and playerBox.x + playerBox.width/2 + playerBox.vx*dt > x2 then
-        playerBox.vx = 0
-        playerBox.x = x2 - playerBox.width/2
-      elseif x1 < x2 and playerBox.x - playerBox.width/2 >= x2
-          and playerBox.x - playerBox.width/2 + playerBox.vx*dt < x2 then
-        playerBox.vx = 0
-        playerBox.x = x2 + playerBox.width/2
-      end
-    end
-
-    -- Sharp corner (acts like a wall)
-    if x1 > x2 and playerBox.x + playerBox.width/2 > x1
-        and playerBox.x - playerBox.width/2 + playerBox.vx*dt < x1 then
-      if (y1 > y2 and playerBox.y > y1
-          and playerBox.y - playerBox.height < y1) or (y1 < y2
-          and playerBox.y > y1 and playerBox.y - playerBox.height < y1) then
-        playerBox.vx = 0
-        playerBox.x = x1 + playerBox.width/2
-      end
-    elseif x1 < x2 and playerBox.x - playerBox.width/2 < x1
-        and playerBox.x + playerBox.width/2 + playerBox.vx*dt > x1 then
-      if (y1 > y2 and playerBox.y > y1
-          and playerBox.y - playerBox.height < y1) or (y1 < y2
-          and playerBox.y > y1 and playerBox.y - playerBox.height < y1) then
-        playerBox.vx = 0
-        playerBox.x = x1 - playerBox.width/2
-      end
-    end
-
-    -- Top and bottom
-    if playerBox.x + playerBox.width/2 > math.min(x1, x2)
-        and playerBox.x - playerBox.width/2 < math.max(x1, x2) then
-      local m = (y2-y1) / (x2-x1)
-
-      if y1 < y2 then
-        -- Ceiling (leaning)
-        if playerBox.y - playerBox.height >= y1
-            and playerBox.y - playerBox.height <= y2 then
-          slopeX = x1 + (playerBox.y-playerBox.height-y1)/m
-
-          if x1 > x2 and playerBox.x - playerBox.width/2 >= slopeX
-              and playerBox.x - playerBox.width/2 + playerBox.vx*dt < slopeX
-              then
-            playerBox.vx = 0
-            playerBox.x = slopeX + playerBox.width/2
-          elseif x1 < x2 and playerBox.x + playerBox.width/2 <= slopeX
-              and playerBox.x + playerBox.width/2 + playerBox.vx*dt > slopeX
-              then
-            playerBox.vx = 0
-            playerBox.x = slopeX - playerBox.width/2
-          end
-        end
-
-        local slopeY = y1 + m*(playerBox.x-x1)
-
-        if playerBox.y - playerBox.height >= slopeY
-            and playerBox.y - playerBox.height + playerBox.vy*dt < slopeY
-            then
-          playerBox.vy = 0
-          playerBox.y = slopeY + playerBox.height
-        end
-
-        -- Floor (flat)
-        if playerBox.y + playerBox.vy*dt > y1 and playerBox.y <= y1 then
-          playerBox.vy = 0
-          playerBox.y = y1
-        end
-
-      else  -- (y1 >= y2)
-
-        -- Ceiling (flat) 
-        if playerBox.vy < 0 and playerBox.y - playerBox.height >= y1
-            and playerBox.y - playerBox.height + playerBox.vy*dt < y1 then
-          playerBox.vy = 0
-          playerBox.y = y1 + playerBox.height
-        end
-
-        -- Floor (leaning)
-        local slopeY = y1 + m*(playerBox.x-x1)
-        if playerBox.y <= math.max(y2, slopeY)
-            and playerBox.y + playerBox.vy*dt > math.max(y2, slopeY) then
-          playerBox.vy = 0
-
-          -- Prevent floating character at slope corner
-          if (playerBox.x-x1)*(playerBox.x-x2) < 0 then
-            playerBox.y = slopeY
-          else
-            playerBox.y = y2
-          end
-        end
-
-        if playerBox.y <= y1 and playerBox.y > math.max(y2, slopeY)
-            and ((x1 > x2 and playerBox.x - playerBox.width/2 <= x1
-                 and playerBox.x + playerBox.width/2 >= x2)
-                 or (x1 < x2 and playerBox.x + playerBox.width/2 >= x1
-                 and playerBox.x - playerBox.width/2 <= x2)) then
-          playerBox.vy = 0
-          playerBox.y = slopeY
-          guyOnSlope = true
-        end
-
-
-      end -- if y1 < y2
-    end -- if playerBox.x + playerBox.width/2 > math.min(x1, x2)
-
-  end  -- for i
-
-  -- Clouds
-  for i in pairs(map.terrain.clouds) do
-    local clouds = map.terrain.clouds[i]
-    local x1 = math.min(clouds[1], clouds[3])
-    local y1 = clouds[2]
-    local x2 = math.max(clouds[1], clouds[3])
-
-    if playerBox.x + playerBox.width/2 > x1 and playerBox.x - playerBox.width/2 < x2
-        and playerBox.y + playerBox.vy*dt > y1 and playerBox.y <= y1
-        and playerBox.vy > 0 then
-      playerBox.vy = 0
-      playerBox.y = y1
-    end
-  end
-
-  local winWidth, winHeight = love.window.getMode()
-  playerBox.x = (playerBox.x+playerBox.vx*dt)
-  playerBox.y = (playerBox.y+playerBox.vy*dt)
+  collision.terrainCollision(components, map.terrain, dt)
 end
 
 function love.draw()
+  local playerBox = components.collisionBoxes.megasapi
   love.graphics.setColor(1, 1, 1)
   player1:draw()
   player2:draw()
