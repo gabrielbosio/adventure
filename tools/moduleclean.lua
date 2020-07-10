08856b27eccd4d000034444367a0e42f306e5322
fbosio <fedebosio93@gmail.com>
2020-07-10 18:07:10 -0300

    moduleclean: help added, now arg[1] is required

--- tools/moduleclean.lua
+++ tools/moduleclean.lua
@@ -1,14 +1,23 @@
 --- Replace Lua 5.1 module function with "The Basic Approach" (PiL 4th ed)
 -- This script assumes that module variables and functions are declared global,
 -- and that their declarations start immediatly, without spaces, comments, and
--- weird stuff. If you don't link this coding style restriction, I don't care.
+-- weird stuff. If you don't like this style restriction, it's your problem.
 -- @author Federico A. Bosio
 -- @script moduleclean
+-- @usage
+local usage = [[Usage: moduleclean.lua <luaFile> [<output>]
+  Default <output> is stdout]]
 
 local deprecated = "^module%(%s*[^,]+(,%s*package.seeall%s*%)?)"
 local foundDeprecated = false
 
-io.input(arg[1])
+local input = arg[1]
+if not input then
+  print(usage)
+  return
+end
+io.input(input)
+io.output(arg[2])  -- to stdout if nil
 
 for line in io.lines() do
   -- Look for the deprecated pattern
