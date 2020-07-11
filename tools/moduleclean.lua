39cb9034ffa96e01fad95c7f195f25c6482d7c7d
fbosio <fedebosio93@gmail.com>
2020-07-10 21:54:35 -0300

    moduleclean: assign require calls to local tables

--- tools/moduleclean.lua
+++ tools/moduleclean.lua
@@ -1,15 +1,14 @@
 --- Replace Lua 5.1 module function with "The Basic Approach" (PiL 4th ed)
 -- This script assumes that module variables and functions are declared global,
 -- and that their declarations start immediatly, without spaces, comments, and
--- weird stuff. If you don't like this style restriction, it's your problem.
+-- weird stuff. So it's an almost useless script, because it doesn't really
+-- analyze the code nor extract variable names. If that's what you want, maybe
+-- writing your own script for your own needs would be better.
 -- @author Federico A. Bosio
 -- @script moduleclean
 -- @usage
 local usage = [[Usage: moduleclean.lua <luaFile> [<output>]
   Default <output> is stdout]]
-
-local deprecated = "^module%(%s*[^,]+(,%s*package.seeall%s*%)?)"
-local foundDeprecated = false
 
 local input = arg[1]
 if not input then
@@ -17,24 +16,36 @@ if not input then
   return
 end
 io.input(input)
-output = ""
 
+local output = ""
+local deprecated = "^module%(%s*[^,]+,%s*package.seeall%s*%)"
+local deprecationsNum = 0
 for line in io.lines() do
   -- Look for the deprecated pattern
-  if not foundDeprecated then
-    local s, n = string.gsub(line, deprecated, "local M = {}")
-    output = output .. s .. "\n"
-    foundDeprecated = n > 0
+  local addedOutput
+  if deprecationsNum == 0 then
+    addedOutput, deprecationsNum = string.gsub(line, deprecated, "local M = {}")
   else
     -- Add nonlocal fields to the module table
-    local s = string.gsub(string.gsub(line, "^(function%s+)", "%1M."),
+    addedOutput = string.gsub(string.gsub(line, "^(function%s+)", "%1M."),
       "^(%S+%s*=)", "M.%1")
-    output = output .. s .. "\n"
   end
+
+  -- Look for global require statements
+  local m, n = string.gsub(addedOutput, '^require%s*%(?"(.+)".*$', "%1")
+  if n > 0 then
+    -- and make them local, when found
+    addedOutput = string.format('local %s = require "%s"',
+      string.gsub(m, ".+%.([^.]+)$", "%1"), m)
+  end
+
+
+  output = output .. addedOutput .. "\n"
 end
 
-if foundDeprecated then
-  io.output(arg[2])  -- to stdout if nil
-  io.write(output, "\nreturn M\n")
+if deprecationsNum > 0 then
+  output = output .. "\nreturn M\n"
 end
 
+io.output(arg[2])  -- to stdout if nil
+io.write(output)
